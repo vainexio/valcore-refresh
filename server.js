95ff6ec49142c923b76e1f5c0892b134e9c1f2a5
Glitch (witty-pinnate-reaper) <none>
2023-04-08 00:46:09 +0000

    🎆🍼 Checkpoint ./server.js:40274427/178

--- server.js
+++ server.js
@@ -755,8 +755,9 @@ client.on("messageCreate", async (message) => {
         .setDescription('\n\n** **')
         .setColor(colors.none)
         let channel = await getChannel(data.channel)
-        !bulked.find(b => b === channel.id) ? await channel.bulkDelete(10) : null
-        bulked.push(channel.id)
+        let foundBulked = bulked.find(b => b === channel.id)
+        !foundBulked ? await channel.bulkDelete(10) : null
+        !foundBulked ? bulked.push(channel.id) : null
         for (let b in data.types) {
           let type = data.types[b]
           let children = ''
@@ -772,7 +773,7 @@ client.on("messageCreate", async (message) => {
       }
     }
     for (let i in bulked) {
-      let channel = bulked[i]
+      let channel = await getChannel(bulked[i])
       let row = new MessageActionRow().addComponents(
           new MessageButton().setLabel('Order Here').setURL('https://discord.com/channels/1047454193159503904/1054711675045036033/1060248361107722290').setStyle('LINK').setEmoji('<:09:1069200736631656518>'))
       await channel.send({components: [row]})
