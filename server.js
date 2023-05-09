2b753578ca034b57def5cfcf73b8760f6e79b387
Glitch (project-scseqdjnsjcdbvuisef) <none>
2023-05-09 08:34:50 +0000

    🏛🏭 Checkpoint ./server.js:40274427/824

--- server.js
+++ server.js
@@ -1123,19 +1123,24 @@ client.on('interactionCreate', async inter => {
       let foundCat = shop.pricelists.find(c => c.name.toLowerCase().includes('nitro'))
       if (!foundCat) return inter.reply(emojis.x+' Invalid Category: `nitro`')
       let style = 'SECONDARY'
+      let quanStyle = 'PRIMARY'
       foundCat.status = quan > 0 ? 1 : 3
-      stockHolder[0].push(new MessageButton().setCustomId('none').setStyle(style).setLabel('Nitro boost ('+quan+')').setEmoji('<a:nitroboost:1057999297787985960>'))
+      stockHolder[0].push(
+        new MessageButton().setCustomId('none-'+getRandom(1,10000)).setStyle(style).setLabel('Nitro boost').setEmoji('<a:nitroboost:1057999297787985960>'),
+        new MessageButton().setCustomId('none-'+getRandom(1,10000)).setStyle(quanStyle).setLabel(quan.toString())
+      )
       for (let i in arrays) {
         let msg = arrays[i];
         if (arrays.length > 0) {
           let args = await getArgs(msg);
-          let text = args[0].includes(':') ? args.slice(1).join(" ") : msg
-          let emoji = args[0].includes(':') ? args[0] : null
+          //let text = args.slice(2).join(" ")
+          let count = args[1]
+          let emoji = args[0]
+          let text = msg.replace(count,'').replace(emoji,'')
           //if (stockHolder[holderCount].length === 5) holderCount++
-          
-          stockHolder[holderCount].push(new MessageButton().setCustomId("none"+getRandom(1,10000)).setStyle(style).setLabel(' ').setLabel(text));
-          stockHolder[holderCount].push(new MessageButton().setCustomId("none"+getRandom(1,10000)).setStyle(style).setLabel().setEmoji(emoji));
           holderCount++
+          stockHolder[holderCount].push(new MessageButton().setCustomId("none"+getRandom(1,10000)).setStyle(style).setLabel(text).setEmoji(emoji));
+          stockHolder[holderCount].push(new MessageButton().setCustomId("none"+getRandom(1,10000)).setStyle(quanStyle).setLabel(count));
         }
       }
     
