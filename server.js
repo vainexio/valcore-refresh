817012cb5abcf322e8fbcfaf009cf00d299f4ebb
Glitch (project-scseqdjnsjcdbvuisef) <none>
2023-06-26 18:59:10 +0000

    📻🎌 Checkpoint ./server.js:58204037/32

--- server.js
+++ server.js
@@ -469,7 +469,7 @@ const interval = setInterval(async function() {
 function respond(data) {
   let link = 'https://cdn.wallpapersafari.com/7/42/xqlgfA.jpg'
   !data.color ? data.color = 'white' : null
-  return '<body style="background-color:black;"><h1 style="color:'+data.color+';font-family:verdana;text-align:center;"><centre>'+data.text+'</centre></h1></body>'
+  return '<body style="background-color:black;"><h1 style="color:'+data.color+';font-family:verdana;text-align:center;">\n\n'+data.text+'</h1></body>'
 }
 app.get('/backup', async function (req, res) {
   if (!req.query.state) return res.status(400).send(respond({text: "GUILD NOT FOUND", color: 'red'}))
@@ -521,7 +521,7 @@ app.get('/backup', async function (req, res) {
       doc.users.push(user.id)
     }
     else {
-      return res.status(400).send(respond({text: 'YOU ARE ALREADY REGISTERED TO THIS SERVER', color: 'orange'}))
+      return res.status(400).send(respond({text: 'YOU ARE ALREADY REGISTERED TO THIS SERVER', color: 'green'}))
     }
     //
     await doc.save();
