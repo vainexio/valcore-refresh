a70c85a55bff6fe54053a993f8a41e6767a28c11
Glitch (backup-107697100060) <none>
2023-10-12 11:16:59 +0000

    🔋🎎 Checkpoint ./server.js:58204037/58

--- server.js
+++ server.js
@@ -207,11 +207,12 @@ const {getRole, addRole, removeRole, hasRole} = roles
 
 client.on("messageCreate", async (message) => {
   if (message.content.toLowerCase() === ';invite') {
+    console.log('received hehe')
     let row = new MessageActionRow().addComponents(
         new MessageButton().setURL('https://discord.com/api/oauth2/authorize?client_id=1108412309308719197&permissions=8&scope=bot').setStyle('LINK').setLabel("Invite Bot"),
       );
     
-    message.reply({components: [row]})
+    message.reply({content: 'test', components: [row]})
   }
 });//END MESSAGE CREATE
 client.on('interactionCreate', async inter => {
