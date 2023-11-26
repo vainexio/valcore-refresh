fbff0c490be5baa775b89e5e40e48183ffb851b7
Glitch (backup-107697100060) <none>
2023-11-26 10:40:38 +0000

    🍃🔑 Checkpoint ./server.js:58204037/494 ./output.html:58204037/7381

--- server.js
+++ server.js
@@ -655,14 +655,15 @@ const interval = setInterval(async function() {
 },21600000) //
 
 function respond(data) {
-  const htmlTemplate = fs.readFileSync('path/to/template.html', 'utf8');
+  const htmlTemplate = fs.readFileSync('output.html', 'utf8');
 
   // Replace placeholders with dynamic values
+  console.log(data.guild.iconURL(),'icon')
   const modifiedHtml = htmlTemplate
-  .replace('${pageTitle}', data.guild.iconURL())
-  .replace('${imageUrl}', 'path/to/dynamic-image.png')
+  .replace('${pageTitle}', data.guild.name)
+  .replace('${imageUrl}', data.guild.iconURL() ? data.guild.iconURL() : 'https://images-ext-1.discordapp.net/external/4vOerAC0lF1iBFoyvX6e_YBijSjc92mdFZEaTABBi0w/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/1108412309308719197/f2c803df2c33edb9faffe59eeaf25827.png?format=webp&width=671&height=671')
   .replace('${subtext}',data.text)
-
+  .replace('${subtextColor}', data.color)
   // Send the modified HTML as the response
   return modifiedHtml;
 }
